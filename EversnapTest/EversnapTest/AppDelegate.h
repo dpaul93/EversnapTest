142f9c80959a1835019415fc455706fd2a16d5e4
Pavlo Deynega <paul.deynega@migonsoftware.com>
2016-08-23 22:54:19 +0300

    Initial commit.

---
+++ EversnapTest/EversnapTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EversnapTest
+//
+//  Created by Pavlo Deynega on 23.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
