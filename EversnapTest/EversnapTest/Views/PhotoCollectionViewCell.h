3eed1b6f1d8fe44d444cd101c419b28f690a87ce
Pavlo Deynega <paul.deynega@migonsoftware.com>
2016-08-24 17:26:47 +0300

    Added photo collection view cell.

---
+++ EversnapTest/EversnapTest/Views/PhotoCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.h
+//  EversnapTest
+//
+//  Created by Pavlo Deynega on 24.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ EversnapTest/EversnapTest/Views/PhotoCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewCell.m
+//  EversnapTest
+//
+//  Created by Pavlo Deynega on 24.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+
+#import "PhotoCollectionViewCell.h"
+
+@implementation PhotoCollectionViewCell
+
+@end
