681eafb7b227f6794705d68b366f373b65584994
Pavlo Deynega <paul.deynega@migonsoftware.com>
2016-08-24 17:27:33 +0300

    Added to method to load photo data with pagination.

---
+++ EversnapTest/EversnapTest/WebService/DTOWrapper/DTOWrapper.m
@@ -0,0 +1,32 @@
+//
+//  DTOWrapper.m
+//
+//  Created by Pavlo Deynega on 02.08.16.
+//  Copyright © 2016 Paul Deynega. All rights reserved.
+//
+
+#import "DTOWrapper.h"
+#import "BaseDTO.h"
+#import "WebService.h"
+
+NSString * const kFlickrAPIKey = @"c472fb63bccdde81de67b841f1a00d70";
+
+@implementation DTOWrapper
+
++(BaseDTO *)getPhotosWithBirthdayTagPage:(NSInteger)page {
+    BaseDTO *baseDTO = [BaseDTO initWithBlock:^(BaseDTO *dto) {
+        dto.requestType = BaseDTORequestGET;
+        dto.urlEndpoint = @"rest/";
+        
+        [dto addValue:@"flickr.photos.search" key:@"method"];
+        [dto addValue:kFlickrAPIKey key:@"api_key"];
+        [dto addValue:@"birthday" key:@"tags"];
+        [dto addValue:@(page) key:@"page"];
+        [dto addValue:@"json" key:@"format"];
+        [dto addValue:@(1) key:@"nojsoncallback"];
+    }];
+    
+    return baseDTO;
+}
+
+@end
