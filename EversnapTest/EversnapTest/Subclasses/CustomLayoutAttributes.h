f46ad7aed7232a39d833d44c155ab49f40369620
Pavlo Deynega <paul.deynega@migonsoftware.com>
2016-08-27 15:17:36 +0300

    Added custom layout attribute to check if attribute is reusable header.

---
+++ EversnapTest/EversnapTest/Subclasses/CustomLayoutAttributes.h
@@ -0,0 +1,15 @@
+//
+//  CustomLayoutAttributes.h
+//  EversnapTest
+//
+//  Created by Pavlo Deynega on 25.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomLayoutAttributes : UICollectionViewLayoutAttributes
+
+@property (assign, nonatomic, getter=isHeader) BOOL header;
+
+@end
---
+++ EversnapTest/EversnapTest/Subclasses/CustomLayoutAttributes.m
@@ -0,0 +1,13 @@
+//
+//  CustomLayoutAttributes.m
+//  EversnapTest
+//
+//  Created by Pavlo Deynega on 25.08.16.
+//  Copyright © 2016 Pavlo Deynega. All rights reserved.
+//
+
+#import "CustomLayoutAttributes.h"
+
+@implementation CustomLayoutAttributes
+
+@end
